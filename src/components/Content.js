de0ddaa1b06aef80a4bcc3f0ed252b767ca2606a
dymafr <erwan1309@hotmail.com>
2022-06-09 14:57:27 +0200

    1 file modified

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import styles from './Content.module.scss';
 
 function Content() {
-  return <h1 className="flex-fill b3">Content</h1>;
+  return (
+    <div className="flex-fill container p-20">
+      <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
+      <div className={`card p-20 ${styles.contentCard}`}>
+        <div className={styles.grid}>
+          <div>Element</div>
+          <div>Element</div>
+          <div>Element</div>
+          <div>Element</div>
+          <div>Element</div>
+          <div>Element</div>
+          <div>Element</div>
+          <div>Element</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Content;
