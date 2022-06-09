02f366f3303e7ad5136a94b88276f04402a5769e
dymafr <erwan1309@hotmail.com>
2022-06-09 15:00:33 +0200

    2 files modified

--- src/components/Content.js
+++ src/components/Content.js
@@ -7,14 +7,16 @@ function Content() {
       <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
       <div className={`card p-20 ${styles.contentCard}`}>
         <div className={styles.grid}>
-          <div>Element</div>
-          <div>Element</div>
-          <div>Element</div>
-          <div>Element</div>
-          <div>Element</div>
-          <div>Element</div>
-          <div>Element</div>
-          <div>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
+          <div className={styles.elem}>Element</div>
         </div>
       </div>
     </div>
