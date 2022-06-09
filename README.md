258dfdc301610bf64d158cbe8bb5077a00dd9a69
dymafr <erwan1309@hotmail.com>
2022-06-09 14:08:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-jemk4w
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-jemk4w)
---
+++ src/components/Content.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Content() {
+  return <h1 className="flex-fill b3">Content</h1>;
+}
+
+export default Content;
